03d5a1377d1374ec679f6226a7c2c4e868721af8
Adrian Candel Prestel <90768238+acandelp@users.noreply.github.com>
2025-04-23 12:44:26 +0200

    Update HCP.sh

--- HCP.sh
+++ HCP.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+export PATH=${HOME}/.local/bin/backplane/latest:${PATH}
+ocm login --use-auth-code --url production
 
+echo Please introduce the Cluster ID:
+read CLUSTERID
+echo The Cluster ID is $CLUSTERID
+echo "Login into the cluster"
+ocm backplane login $CLUSTERID
+MC_ID=$(ocm describe cluster $CLUSTERID | grep Management | awk '{ print $3 }')
+echo The ManagementCluster ID is: $MC_ID
+INT_ID=$(ocm describe cluster $CLUSTERID | grep "ID" -m1 | awk '{ print $2}')
+echo The Internal ID is: $INT_ID
+echo Backplane to the MC
+ocm backplane login $MC_ID
+echo "List the namespaces from the customer cluster inside the MC"
+oc get project | grep $INT_ID
